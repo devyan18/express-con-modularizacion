b697bc65ce9fa9ffee898ef9241d0ec4b7e76b0c
bogaclau <sebba30015boga@gmail.com>
2023-09-04 20:04:43 -0300

    se agrego el middleware de morgan

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
 // middleware
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 
 // routes
